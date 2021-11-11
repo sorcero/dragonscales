f2804992026bc2853b3e4e5621518fced7d410c0
Martin Abente Lahaye <martin@sorcero.com>
2021-11-11 14:25:44 -0300

    dragonscales-worker: Use DRAGONSCALES_QUEUE_URL and remove unused ones

--- dragonscales-worker.sh
+++ dragonscales-worker.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-export DRAGONSCALES_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 export DRAGONSCALES_QUEUE_NAME="${DRAGONSCALES_QUEUE_NAME:-default}"
 export DRAGONSCALES_QUEUE_URL="${DRAGONSCALES_QUEUE_URL:-redis://localhost:6379}"
 
-rq worker $DRAGONSCALES_QUEUE_NAME
+rq worker --url $DRAGONSCALES_QUEUE_URL $DRAGONSCALES_QUEUE_NAME
